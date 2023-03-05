9fe4ecd589b687fe7bf3a938d487f91a36c82cff
BOUALI Ali <bouali.pro@gmail.com>
2023-03-05 02:34:02 -0800

    Update BootcampApplication.java

--- src/main/java/com/alibou/bootcamp/BootcampApplication.java
+++ src/main/java/com/alibou/bootcamp/BootcampApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootcampApplication {
+	// Fix conflict
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampApplication.class, args);
 	}
