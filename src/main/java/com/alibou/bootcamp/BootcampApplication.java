985404930204841eda6709af6c0004f7e9b6795d
alibouali <ali.bouali@mimacom.com>
2023-03-05 11:18:34 +0100

    add comment

--- src/main/java/com/alibou/bootcamp/BootcampApplication.java
+++ src/main/java/com/alibou/bootcamp/BootcampApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootcampApplication {
-
+	// some comment
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampApplication.class, args);
 	}
