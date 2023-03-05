d398b4da7b621c4684b92471e825971cb1549704
ali-bouali <bouali.pro@gmail.com>
2023-03-05 11:30:33 +0100

    remove comment

--- src/main/java/com/alibou/bootcamp/BootcampApplication.java
+++ src/main/java/com/alibou/bootcamp/BootcampApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootcampApplication {
-	// some comment
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampApplication.class, args);
 	}
