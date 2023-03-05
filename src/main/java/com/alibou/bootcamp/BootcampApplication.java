d396b078665157309ac63b39af57b2fa2c1d80d3
alibouali <ali.bouali@mimacom.com>
2023-03-05 11:15:22 +0100

    first commit

---
+++ src/main/java/com/alibou/bootcamp/BootcampApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampApplication.class, args);
+	}
+
+}
