9bc027362ed8c44a99fa61b59db82cd3b090da8d
ali-bouali <bouali.pro@gmail.com>
2023-03-12 10:13:04 +0100

    switching to main bootcamp application

--- src/main/java/com/alibou/bootcamp/student/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alibou.bootcamp.student;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
