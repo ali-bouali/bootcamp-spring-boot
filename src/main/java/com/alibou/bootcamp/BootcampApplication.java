a2736ac47784e53d3efce36055e64744bf78201a
ali-bouali <bouali.pro@gmail.com>
2023-08-20 13:50:31 +0200

    trying out ELK

--- src/main/java/com/alibou/bootcamp/BootcampApplication.java
+++ src/main/java/com/alibou/bootcamp/BootcampApplication.java
@@ -2,12 +2,14 @@ package com.alibou.bootcamp;
 
 import com.alibou.bootcamp.account.AccountRequest;
 import com.alibou.bootcamp.account.AccountService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class BootcampApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampApplication.class, args);
@@ -18,6 +20,7 @@ public class BootcampApplication {
 			AccountService service
 	) {
 		return args -> {
+			log.info("The application has started");
 			// service.save(AccountRequest.builder().build());
 		};
 	}
